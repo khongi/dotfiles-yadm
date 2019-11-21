4caa024573851a58c76104d4a73f0ec622a7687f
Khongor Bayarsaikhan <b.khongi@gmail.com>
2019-11-21 23:50:32 +0100

    Add common zsh configs

---
+++ custom/sources/zsh_common.sh
@@ -0,0 +1,15 @@
+# Common configuration for .zshrc
+#
+# Khongor Bayarsaikhan - 2019
+
+# Use lf to switch directories and bind it to ctrl-o
+lfcd () {
+    tmp="$(mktemp)"
+    lf -last-dir-path="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+    fi
+}
+bindkey -s '^o' 'lfcd\n'
